2fc180fe5c334d431649b55a88964a493b7daf8a
Anastasia Orlova <anastasia2804@gmail.com>
2022-09-13 17:40:50 -0400

    Iterations 1, 2 completed

--- app.js
+++ app.js
@@ -5,23 +5,39 @@ require('dotenv/config');
 // ℹ️ Connects to the database
 require('./db');
 
-// Handles http requests (express is node js framework)
-// https://www.npmjs.com/package/express
 const express = require('express');
-
 const bodyParser = require('body-parser')
-
-// Handles the handlebars
-// https://www.npmjs.com/package/hbs
-const hbs = require('hbs');
+const session = require('express-session') 
+const MongoStore = require('connect-mongo');
+// const mongoose = require('mongoose');
+// const hbs = require('hbs');
 
 const app = express();
 
-
-app.use(bodyParser.urlencoded({ extended: false }))
-
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
 require('./config')(app);
+
+// required for the app when deployed to Heroku (in production)
+app.set('trust proxy', 1);
+ 
+// use session
+app.use(
+    session({
+      secret: process.env.SESS_SECRET,
+      resave: true,
+      saveUninitialized: false,
+      cookie: {
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+        secure: process.env.NODE_ENV === 'production',
+        httpOnly: true,
+        maxAge: 60000 // 60 * 1000 ms === 1 min
+      },
+      store: MongoStore.create({
+        mongoUrl: "mongodb://localhost/lab-express-basic-auth"
+      })
+    })
+  );
+
 
 // default value for title local
 const projectName = 'lab-express-basic-auth';
--- routes/index.js
+++ routes/index.js
@@ -32,4 +32,57 @@ User.create({
   })
 })
 
+//GET login route
+router.get('/login', (req, res, next) => {
+  res.render('login.hbs');
+})
+
+
+//POSt login route
+router.post('/login', (req, res, next) => {
+  console.log('SESSION =====> ', req.session);
+
+  const myUsername = req.body.username;
+  const myPassword = req.body.password;
+
+User.findOne({
+  username: myUsername
+})
+.then(foundUser => {
+
+  //user not found
+  if(!foundUser) {
+    res.send('no user matching this username');
+    return;
+  }
+
+//compare passwords
+const isValidPassword = bcryptjs.compareSync(myPassword, foundUser.password)
+
+//password is invalid
+ if(!isValidPassword){
+  res.send('incorrect password');
+  return;
+ }
+
+ req.session.user = foundUser;
+
+      res.redirect('/profile')
+
+ })
+ .catch(err => {
+  res.send(err)
+})
+})
+
+router.get('/profile', (req, res, next) => {
+  console.log(req.session.user)
+  if(req.session.user){
+    res.render('profile.hbs', { username: req.session.user.username})
+  } else {
+    res.render('profile.hbs', { username: 'Anonymous' });
+  }
+  
+});
+
 module.exports = router;
